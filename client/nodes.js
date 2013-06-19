b7907d3278b94d3b737ea454b4f4458a576fd9f7
nanusdad <arun@zechariah.net>
2013-06-19 13:47:25 -0400

    Update nodes.js
    autorun not required

--- client/nodes.js
+++ client/nodes.js
@@ -2,10 +2,11 @@
     Meteor.subscribe("servers", function() {
 	console.log("subscribed");
     });
-    return Meteor.autorun(function() {
+/*    return Meteor.autorun(function() {
       items = _(Servers.find().fetch()).pluck("name");
       return console.log('items', items);
     });
+*/
   });
   Template.search.rendered = function() {
     return $('.typeahead').typeahead({
